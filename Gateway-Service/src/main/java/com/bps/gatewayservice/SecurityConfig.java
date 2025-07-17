c0fc046d08efcc3170d124136012b9f04ebe742f
Mr Skylar <64327510+skylar69-wtf@users.noreply.github.com>
2025-07-17 14:41:49 +0530

    Added Security Feature

---
+++ Gateway-Service/src/main/java/com/bps/gatewayservice/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.bps.gatewayservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())  // Hosted login
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt()); // JWT validation
+
+        return http.build();
+    }
+}
+
